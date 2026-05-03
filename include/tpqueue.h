104f164dd02b76fa6cd749da4a16eab42826ebb5
morosArtem <morosart1995@gmail.com>
2026-05-03 15:44:13 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,15 +1,77 @@
 // Copyright 2022 NNTU-CS
-#ifndef INCLUDE_TPQUEUE_H_
-#define INCLUDE_TPQUEUE_H_
+#ifndef TPQUEUE_H_
+#define TPQUEUE_H_
 
-template<typename T>
-class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
-};
-
-struct SYM {
+struct Sym {
   char ch;
   int prior;
 };
 
-#endif  // INCLUDE_TPQUEUE_H_
+template<typename T>
+class TPQueue {
+ private:
+  struct Node {
+    T data;
+    Node* next;
+  };
+
+  Node* head;
+  int count;
+
+ public:
+  TPQueue() : head(nullptr), count(0) {}
+
+  ~TPQueue() {
+    while (!isEmpty()) {
+      pop();
+    }
+  }
+
+  void push(const T& item) {
+    Node* newNode = new Node;
+    newNode->data = item;
+    newNode->next = nullptr;
+
+    if (isEmpty() || item.prior > head->data.prior) {
+      newNode->next = head;
+      head = newNode;
+    } else {
+      Node* current = head;
+      while (current->next != nullptr && current->next->data.prior >= item.prior) {
+        current = current->next;
+      }
+      newNode->next = current->next;
+      current->next = newNode;
+    }
+    count++;
+  }
+
+  T pop() {
+    if (isEmpty()) {
+      throw "Queue is empty";
+    }
+    Node* temp = head;
+    T result = head->data;
+    head = head->next;
+    delete temp;
+    count--;
+    return result;
+  }
+
+  T front() const {
+    if (isEmpty()) {
+      throw "Queue is empty";
+    }
+    return head->data;
+  }
+
+  bool isEmpty() const {
+    return head == nullptr;
+  }
+
+  int size() const {
+    return count;
+  }
+};
+
+#endif  // TPQUEUE_H_
